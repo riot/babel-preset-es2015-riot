17d6a0fed6e3f4bc83a70b719f01262383b78486
Mike Nichols <nichols.mike.s@gmail.com>
2016-01-14 22:47:56 -0700

    make require static analyzeable

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-function _r(p) { return require('babel-plugin-' + p) }
-
 module.exports = {
   plugins: [
-   [_r('transform-es2015-template-literals'), {'loose': true}],
-    _r('transform-es2015-literals'),
-    _r('transform-es2015-function-name'),
-    _r('transform-es2015-arrow-functions'),
-    _r('transform-es2015-block-scoped-functions'),
-   [_r('transform-es2015-classes'), {'loose': true}],
-    _r('transform-es2015-object-super'),
-    _r('transform-es2015-shorthand-properties'),
-   [_r('transform-es2015-computed-properties'), {'loose': true}],
-   [_r('transform-es2015-for-of'), {'loose': true}],
-    _r('transform-es2015-sticky-regex'),
-    _r('transform-es2015-unicode-regex'),
-    _r('check-es2015-constants'),
-   [_r('transform-es2015-spread'), {'loose': true}],
-    _r('transform-es2015-parameters'),
-   [_r('transform-es2015-destructuring'), {'loose': true}],
-    _r('transform-es2015-block-scoping'),
-    _r('transform-es2015-typeof-symbol'),
-   [_r('transform-es2015-modules-commonjs'), { 'allowTopLevelThis': true }],
-   [_r('transform-regenerator'), { 'async': false, 'asyncGenerators': false }]
+   [require('transform-es2015-template-literals'), {'loose': true}],
+    require('transform-es2015-literals'),
+    require('transform-es2015-function-name'),
+    require('transform-es2015-arrow-functions'),
+    require('transform-es2015-block-scoped-functions'),
+   [require('transform-es2015-classes'), {'loose': true}],
+    require('transform-es2015-object-super'),
+    require('transform-es2015-shorthand-properties'),
+   [require('transform-es2015-computed-properties'), {'loose': true}],
+   [require('transform-es2015-for-of'), {'loose': true}],
+    require('transform-es2015-sticky-regex'),
+    require('transform-es2015-unicode-regex'),
+    require('check-es2015-constants'),
+   [require('transform-es2015-spread'), {'loose': true}],
+    require('transform-es2015-parameters'),
+   [require('transform-es2015-destructuring'), {'loose': true}],
+    require('transform-es2015-block-scoping'),
+    require('transform-es2015-typeof-symbol'),
+   [require('transform-es2015-modules-commonjs'), { 'allowTopLevelThis': true }],
+   [require('transform-regenerator'), { 'async': false, 'asyncGenerators': false }]
   ]
 }
